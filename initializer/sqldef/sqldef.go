f52434a7c70785e7fa9fce8c0e62100188e8d1ff
Pablo Lalloni <plalloni@gmail.com>
2018-10-22 13:22:07 -0300

    Refactoriza código para independizar lógica

--- initializer/sqldef/sqldef.go
+++ initializer/sqldef/sqldef.go
@@ -5,14 +5,24 @@ import (
 	"path/filepath"
 
 	"github.com/pkg/errors"
+
 	"gitlab.cloudint.afip.gob.ar/std/std-buildr/config"
 	"gitlab.cloudint.afip.gob.ar/std/std-buildr/git"
+	"gitlab.cloudint.afip.gob.ar/std/std-buildr/initializer/helpers"
 	"gitlab.cloudint.afip.gob.ar/std/std-buildr/initializer/templates"
 )
 
 const srcFolder = "src/sql"
 
 func Initialize(cfg *config.Config) error {
+
+	if err := helpers.ValidateProjectConfig(cfg); err != nil {
+		return errors.Wrap(err, "checking project configuration requirements")
+	}
+
+	if err := helpers.CreateProject(cfg, "evolutivo"); err != nil {
+		return errors.Wrap(err, "creating project structure")
+	}
 
 	if err := os.MkdirAll(srcFolder, 0775); err != nil {
 		return errors.Wrapf(err, "creating directory")
@@ -23,7 +33,7 @@ func Initialize(cfg *config.Config) error {
 		return errors.Wrapf(err, "creating %s", p)
 	}
 
-	if err := git.CommitAddingAll("Crea estructura de proyecto diferido (std-buildr)"); err != nil {
+	if err := git.CommitAddingAll("Crea estructura inicial de SQL diferido (std-buildr)"); err != nil {
 		return errors.Wrap(err, "creating sql deferred commit in git")
 	}
 
